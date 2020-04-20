9858072f275d6591123a4ccd71f937092bf215ba
Rafael Alfaro <rafalfaro_18@outlook.com>
2020-04-19 23:21:47 -0600

    Updates

---
+++ precache-manifest.82f4dee370235ac0edefdb8b3f7f7805.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "734b70e847fb3f5c6486b2626e101b0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "936bff8a101c6eeac7c7",
+    "url": "/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "fd0673ece7359b042d63",
+    "url": "/static/js/2.0602a6f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0602a6f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "936bff8a101c6eeac7c7",
+    "url": "/static/js/main.e07bde07.chunk.js"
+  },
+  {
+    "revision": "74e49ac7e891cad45815",
+    "url": "/static/js/runtime-main.ef79c2fe.js"
+  }
+]);
