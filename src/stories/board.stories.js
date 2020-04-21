6220e2ecc89055a44e53ef97cc5f95ecd2bf2103
Rafael Alfaro <rafalfaro_18@outlook.com>
2020-04-20 20:26:15 -0600

    Adds completed board story

--- src/stories/board.stories.js
+++ src/stories/board.stories.js
@@ -4,3 +4,4 @@ import { Board } from '../index';
 export default { title: 'Board' };
 
 export const empty = () => <Board squares={Array(9).fill(null)} />;
+export const completed = () => <Board squares={["O","X","O","O","X","X","X","O","X"]} />;
