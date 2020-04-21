fe148e97654f88b250f89897df0ac9740d3c10f3
Rafael Alfaro <rafalfaro_18@outlook.com>
2020-04-20 20:45:38 -0600

    Adds square actions to board story

--- src/stories/board.stories.js
+++ src/stories/board.stories.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { action } from '@storybook/addon-actions';
 import { Board } from '../index';
 
 export default { title: 'Board' };
 
-export const empty = () => <Board squares={Array(9).fill(null)} />;
-export const completed = () => <Board squares={["O","X","O","O","X","X","X","O","X"]} />;
+export const empty = () => <Board squares={Array(9).fill(null)} onClick={action('Square clicked')} />;
+export const completed = () => <Board squares={["O","X","O","O","X","X","X","O","X"]} onClick={action('Square clicked')} />;
